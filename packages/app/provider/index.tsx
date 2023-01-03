557e05b46c899e3199609f91ee3584c8ba752e3a
deggertsen <david@latitude.io>
2023-01-02 22:44:51 -0500

    navigation provider setup

--- packages/app/provider/index.tsx
+++ packages/app/provider/index.tsx
@@ -1,11 +1,14 @@
 import config from '../tamagui.config'
 import { NavigationProvider } from './navigation'
 import { TamaguiProvider, TamaguiProviderProps } from '@my/ui'
+import { useNavigationContainerRef } from '@react-navigation/native'
+import { RootStackParamList } from './navigation/types'
 
 export function Provider({ children, ...rest }: Omit<TamaguiProviderProps, 'config'>) {
+  const navigationRef = useNavigationContainerRef<RootStackParamList>()
   return (
-    <TamaguiProvider config={config} disableInjectCSS defaultTheme="light" {...rest}>
-      <NavigationProvider>{children}</NavigationProvider>
+    <TamaguiProvider config={config} disableInjectCSS defaultTheme="DungeonVanilla" {...rest}>
+      <NavigationProvider navigationRef={navigationRef}>{children}</NavigationProvider>
     </TamaguiProvider>
   )
 }
