20f93e50c7cea33892ef2966b8edc98f8e51af66
deggertsen <david@latitude.io>
2023-01-02 22:25:39 -0500

    Set prefixes for ai dungeon web

--- packages/app/provider/navigation/index.tsx
+++ packages/app/provider/navigation/index.tsx
@@ -1,5 +1,4 @@
 import { NavigationContainer } from '@react-navigation/native'
-import * as Linking from 'expo-linking'
 import { useMemo } from 'react'
 
 export function NavigationProvider({
@@ -11,7 +10,12 @@ export function NavigationProvider({
     <NavigationContainer
       linking={useMemo(
         () => ({
-          prefixes: [Linking.createURL('/')],
+          prefixes: [
+            'https://play.aidungeon.io', // ai-dungeon-app production
+            'https://beta.aidungeon.io', // ai-dungeon-app staging
+            'https://ai-dungeon-app-main.netlify.app', // ai-dungeon-app staging
+            'http://localhost:3001', // ai-dungeon-app local web
+          ],
           config: {
             initialRouteName: 'home',
             screens: {
