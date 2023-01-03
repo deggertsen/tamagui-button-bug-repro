82f5cc763ca5265aebee7589272ba31aab2b0ea0
deggertsen <david@latitude.io>
2023-01-03 17:32:33 -0500

    Add auth context to providers

--- packages/app/provider/index.tsx
+++ packages/app/provider/index.tsx
@@ -1,11 +1,12 @@
 import config from '../tamagui.config'
 import { NavigationProvider } from './navigation'
 import { TamaguiProvider, TamaguiProviderProps } from '@my/ui'
+import AuthContext from './auth/AuthContext'
 
 export function Provider({ children, ...rest }: Omit<TamaguiProviderProps, 'config'>) {
   return (
     <TamaguiProvider config={config} disableInjectCSS defaultTheme="light" {...rest}>
-      <NavigationProvider>{children}</NavigationProvider>
+      <NavigationProvider><AuthContext>{children}</AuthContext></NavigationProvider>
     </TamaguiProvider>
   )
 }
