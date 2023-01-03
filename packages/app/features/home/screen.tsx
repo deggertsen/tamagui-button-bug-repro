16185e3d3c9a874bd00917dcef418d359b6a8435
deggertsen <david@latitude.io>
2023-01-03 15:10:13 -0500

    Don't use demo content

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -1,82 +1,30 @@
-import { Anchor, Button, H1, Input, Paragraph, Separator, Sheet, XStack, YStack } from '@my/ui'
-import { ChevronDown, ChevronUp } from '@tamagui/lucide-icons'
-import React, { useState } from 'react'
-import { useLink } from 'solito/link'
+import { Anchor, H1, Paragraph, XStack, YStack } from '@my/ui'
+import { ChevronRight } from '@tamagui/lucide-icons'
+import React from 'react'
 
 export function HomeScreen() {
-  const linkProps = useLink({
-    href: '/user/nate',
-  })
 
   return (
     <YStack f={1} jc="center" ai="center" p="$4" space>
-      <YStack space="$4" maw={600}>
-        <H1 ta="center">Welcome to Tamagui.</H1>
-        <Paragraph ta="center">
-          Here's a basic starter to show navigating from one screen to another. This screen uses the
-          same code on Next.js and React Native.
-        </Paragraph>
-
-        <Separator />
-        <Paragraph ta="center">
-          Made by{' '}
-          <Anchor color="$color12" href="https://twitter.com/natebirdman" target="_blank">
-            @natebirdman
-          </Anchor>
-          ,{' '}
-          <Anchor
-            color="$color12"
-            href="https://github.com/tamagui/tamagui"
-            target="_blank"
-            rel="noreferrer"
-          >
-            give it a ⭐️
-          </Anchor>
-        </Paragraph>
-      </YStack>
-
-      <XStack>
-        <Button {...linkProps}>Link to user</Button>
+      <XStack mb="5c">
+        <H1>User{`\n`}Settings</H1>
       </XStack>
-
-      <SheetDemo />
+      <YStack overflow="hidden">
+          <Anchor
+            mb="1px"
+            bg="muted.800"
+            p="3c"
+            href="/login"
+            accessibilityLabel="Login Button"
+            accessibilityHint="log in"
+            style={undefined}
+          >
+            <XStack alignItems="center" justifyContent="space-between">
+              <Paragraph>Login</Paragraph>
+              <ChevronRight color='#828A92' />
+            </XStack>
+          </Anchor>
+      </YStack>
     </YStack>
-  )
-}
-
-function SheetDemo() {
-  const [open, setOpen] = useState(false)
-  const [position, setPosition] = useState(0)
-  return (
-    <>
-      <Button
-        size="$6"
-        icon={open ? ChevronDown : ChevronUp}
-        circular
-        onPress={() => setOpen((x) => !x)}
-      />
-      <Sheet
-        modal
-        open={open}
-        onOpenChange={setOpen}
-        snapPoints={[80]}
-        position={position}
-        onPositionChange={setPosition}
-        dismissOnSnapToBottom
-      >
-        <Sheet.Overlay />
-        <Sheet.Frame ai="center" jc="center">
-          <Sheet.Handle />
-          <Button
-            size="$6"
-            circular
-            icon={ChevronDown}
-            onPress={() => {
-              setOpen(false)
-            }}
-          />
-        </Sheet.Frame>
-      </Sheet>
-    </>
   )
 }
