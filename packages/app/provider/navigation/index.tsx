50c9a38e621c13af50ad4187e11361eede6cfa7e
deggertsen <david@latitude.io>
2023-01-03 16:43:04 -0500

    Revert some changes that was breaking mobile

--- packages/app/provider/navigation/index.tsx
+++ packages/app/provider/navigation/index.tsx
@@ -1,41 +1,27 @@
-import { NavigationContainer, NavigationContainerRefWithCurrent } from '@react-navigation/native'
+import { NavigationContainer } from '@react-navigation/native'
+import * as Linking from 'expo-linking'
+import { useMemo } from 'react'
 
 export function NavigationProvider({
-  navigationRef,
   children,
 }: {
-  navigationRef: NavigationContainerRefWithCurrent<ReactNavigation.RootParamList>
   children: React.ReactNode
 }) {
-  const linking = {
-    prefixes: [
-      'https://play.aidungeon.io', // ai-dungeon-app production
-      'https://beta.aidungeon.io', // ai-dungeon-app staging
-      'https://ai-dungeon-app-main.netlify.app', // ai-dungeon-app staging
-      'http://localhost:19006', // ai-dungeon-app local web
-    ],
-    config: {
-      screens: {
-        home: '',
-      },
-    },
-  }
-  
   return (
     <NavigationContainer
-      ref={navigationRef}
-      theme={{
-        dark: true,
-        colors: {
-          primary: 'rgb(0, 0, 0)',
-          background: 'rgb(0,0,0)',
-          card: 'rgb(18, 18, 18)',
-          text: 'rgb(255,255, 255)',
-          border: 'rgb(19, 19, 19)',
-          notification: 'rgb(0, 0, 0)',
-        },
-      }}
-      linking={linking}
+      linking={useMemo(
+        () => ({
+          prefixes: [Linking.createURL('/')],
+          config: {
+            initialRouteName: 'home',
+            screens: {
+              home: '',
+              'user-detail': 'user/:id',
+            },
+          },
+        }),
+        []
+      )}
     >
       {children}
     </NavigationContainer>
