84552db91713b70cb587aae4b2477a331992f253
deggertsen <david@latitude.io>
2023-01-04 17:48:41 -0500

    Add firebase config for web

---
+++ apps/next/FirebaseConfig.ts
@@ -0,0 +1,19 @@
+import 'env/config'
+
+const apiKey = process.env.REACT_APP_FIREBASE_API_KEY
+const authDomain = process.env.REACT_APP_FIREBASE_AUTH_DOMAIN
+const databaseURL = process.env.REACT_APP_FIREBASE_DATABASE_URL
+const projectId = process.env.REACT_APP_FIREBASE_PROJECT_ID
+const storageBucket = process.env.REACT_APP_FIREBASE_STORAGE_BUCKET
+const messagingSenderId = process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
+
+const firebaseConfig = {
+  apiKey,
+  authDomain,
+  databaseURL,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+}
+
+export default firebaseConfig
