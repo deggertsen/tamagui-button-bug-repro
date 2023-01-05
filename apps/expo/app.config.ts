fc157a355ed9c1ecc274e22f4470595aadd70e53
deggertsen <david@latitude.io>
2023-01-05 15:20:23 -0500

    Spread config defaults

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -1,6 +1,7 @@
 import { ExpoConfig, ConfigContext } from 'expo/config'
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
+    ...config,
     name: "AIDungeon",
     slug: "ai-dungeon",
     scheme: "aidungeon",
