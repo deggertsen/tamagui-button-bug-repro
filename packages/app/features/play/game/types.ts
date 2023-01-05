00858c482ba24f6a1c84a37ee209cd01aeeedf48
deggertsen <david@latitude.io>
2023-01-05 13:03:40 -0500

    Add game types commented out for now

---
+++ packages/app/features/play/game/types.ts
@@ -0,0 +1,4 @@
+// import { AdventureContextGetAdventureQuery } from '../../../generated/graphql'
+
+// export type GameScreenAdventure = AdventureContextGetAdventureQuery['adventure']
+// export type GameScreenActionWindow = GameScreenAdventure['actionWindow'][0]
