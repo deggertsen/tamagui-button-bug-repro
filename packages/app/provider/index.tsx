3367a201d30b94951580c0bee698cf96831123a3
deggertsen <david@latitude.io>
2023-01-05 13:35:07 -0500

    Add apollo context

--- packages/app/provider/index.tsx
+++ packages/app/provider/index.tsx
@@ -2,11 +2,12 @@ import config from '../tamagui.config'
 import { NavigationProvider } from './navigation'
 import { TamaguiProvider, TamaguiProviderProps } from '@my/ui'
 import AuthContext from './auth/AuthContext'
+import ApolloContext from './apollo/ApolloContext'
 
 export function Provider({ children, ...rest }: Omit<TamaguiProviderProps, 'config'>) {
   return (
     <TamaguiProvider config={config} disableInjectCSS defaultTheme="light" {...rest}>
-      <NavigationProvider><AuthContext>{children}</AuthContext></NavigationProvider>
+      <NavigationProvider><AuthContext><ApolloContext>{children}</ApolloContext></AuthContext></NavigationProvider>
     </TamaguiProvider>
   )
 }
