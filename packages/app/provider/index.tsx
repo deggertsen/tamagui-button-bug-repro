457d6713162642286f37833bc837b15cc79d85eb
deggertsen <david@latitude.io>
2023-01-05 19:14:36 -0500

    Remove native base for now.

--- packages/app/provider/index.tsx
+++ packages/app/provider/index.tsx
@@ -4,11 +4,20 @@ import { TamaguiProvider, TamaguiProviderProps } from '@my/ui'
 import AuthContext from './auth/AuthContext'
 import ApolloContext from './apollo/ApolloContext'
 import UserContextProvider from './user/UserContext'
+// import DungeonVanilla from '@my/ui/src/DungeonVanilla'
 
 export function Provider({ children, ...rest }: Omit<TamaguiProviderProps, 'config'>) {
   return (
     <TamaguiProvider config={config} disableInjectCSS defaultTheme="light" {...rest}>
-      <NavigationProvider><AuthContext><ApolloContext><UserContextProvider>{children}</UserContextProvider></ApolloContext></AuthContext></NavigationProvider>
+      <NavigationProvider>
+        <AuthContext>
+          <ApolloContext>
+            <UserContextProvider>
+              {children}
+            </UserContextProvider>
+          </ApolloContext>
+        </AuthContext>
+      </NavigationProvider>
     </TamaguiProvider>
   )
 }
