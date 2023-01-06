e7e2fa06526e764086e3a072bc611be489620511
deggertsen <david@latitude.io>
2023-01-05 19:05:17 -0500

    use is electron package

--- packages/app/helpers/Platform.ts
+++ packages/app/helpers/Platform.ts
@@ -1,4 +1,5 @@
 import { Platform } from 'react-native'
+import getElectron from 'is-electron'
 
 export const platform = Platform.OS
 
@@ -6,5 +7,4 @@ export const isWeb = platform === 'web'
 export const isAndroid = platform === 'android'
 export const isIOS = platform === 'ios'
 export const isMobile = platform !== 'web'
-export const isElectron =
-  platform === 'web' && navigator.userAgent.toLowerCase().includes('electron')
+export const isElectron = getElectron()
